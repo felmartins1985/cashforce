2972de7369e17f9cc75c86948b9edd0b3dde88a7
hp <hp@pop-os.localdomain>
2022-12-30 16:25:02 -0300

    fix: resolvido o problema com o retorno do teste

--- app/back-end/src/tests/cash.test.ts
+++ app/back-end/src/tests/cash.test.ts
@@ -1,26 +1,27 @@
 import * as chai from 'chai';
 import * as sinon from 'sinon';
-import {app} from '../app';
-import * as mocha from 'mocha';
+import { app } from '../app';
+import { describe }  from 'mocha';
+import Orders from '../database/models/OrdersModel';
 // @ts-ignore
 import chaiHttp = require('chai-http');
-import CashModel from '../models/CashModel';
 chai.use(chaiHttp);
 const { expect } = chai;
 import { mock } from './mock';
 
 
   describe('Rota Get / ', () => {
-    // before(async () => {
-    //   sinon.stub(CashModel.prototype,'findAll').resolves(mock);
-    // });
-    // after(() => {
-    //   (CashModel.prototype.findAll as sinon.SinonStub).restore();
-    // });
+    before(async () => {
+      sinon.stub(Orders,'findAll').resolves(mock);
+    });
+    after(() => {
+      (Orders.findAll as sinon.SinonStub).restore();
+    });
     it('Retorna sem nenhum problema ', async () => {
       const result = await chai.request(app).get('/');
       expect(result.status).to.be.equal(200);
       expect(result.body).to.be.an('array');
       expect(result.body).to.deep.equal(mock);
+      expect(result.body).to.have.lengthOf(3);
     });
   });
